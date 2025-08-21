96ea6db8625f6795f7105bf33870b54a7d1646df
Eliecer <eliecerrojas911@gmail.com>
2025-08-21 16:33:02 -0400

    Arreglando bug de imagen principal

--- script.js
+++ script.js
@@ -110,14 +110,7 @@ const observerOptions = {
     rootMargin: '0px 0px -50px 0px'
 };
 
-const observer = new IntersectionObserver(function(entries) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.style.opacity = '1';
-            entry.target.style.transform = 'translateY(0)';
-        }
-    });
-}, observerOptions);
+const observer = new IntersectionObserver();
 
 // Observe menu items for animation
 document.querySelectorAll('.menu-item').forEach(item => {
